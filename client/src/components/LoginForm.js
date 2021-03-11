418ea08ee40948c200a34529f2737e6d8f48d5e5
AnnMarkham <ann.e.markham@outlook.com>
2021-03-11 01:31:08 -0700

    fix Login form and remove unused functions from utils/API.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 //import { loginUser } from '../utils/API';
-import { ADD_USER } from "../utils/mutations";
+import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
 import Auth from '../utils/auth';
 
@@ -29,15 +29,15 @@ const LoginForm = () => {
     }
 
     try {
-      const { data } = await login({
+      const { data } = await loginUser({
         variables: { ...userFormData }
       })
 
-      if (!response.ok) {
+      if (error) {
         throw new Error('something went wrong!');
       }
 
-      Auth.login(token);
+      Auth.loginUser(data.loginUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
